84a4f25b746e85ede4b3c0c455cc1c133e938252
Martin Vondráček <vondracek.mar@gmail.com>
2020-06-23 04:36:13 +0200

    parser: lineno and lexpos does not exist in provided production if tracking is disabled

--- src/slimit/parser.py
+++ src/slimit/parser.py
@@ -387,7 +387,13 @@ class Parser(object):
                      | call_expr PERIOD identifier
         """
         if len(p) == 3:
-            p[0] = ast.FunctionCall(p[1], p[2], lex_lineno=p.slice[1].lineno, lex_lexpos=p.slice[1].lexpos)
+            try:
+                lex_lineno = p.slice[1].lineno
+                lex_lexpos = p.slice[1].lexpos
+            except AttributeError:
+                lex_lineno = None
+                lex_lexpos = None
+            p[0] = ast.FunctionCall(p[1], p[2], lex_lineno=lex_lineno, lex_lexpos=lex_lexpos)
         elif len(p) == 4:
             p[0] = ast.DotAccessor(p[1], p[3])
         else:
@@ -400,7 +406,13 @@ class Parser(object):
                           | call_expr_nobf PERIOD identifier
         """
         if len(p) == 3:
-            p[0] = ast.FunctionCall(p[1], p[2], lex_lineno=p.slice[1].lineno, lex_lexpos=p.slice[1].lexpos)
+            try:
+                lex_lineno = p.slice[1].lineno
+                lex_lexpos = p.slice[1].lexpos
+            except AttributeError:
+                lex_lineno = None
+                lex_lexpos = None
+            p[0] = ast.FunctionCall(p[1], p[2], lex_lineno=lex_lineno, lex_lexpos=lex_lexpos)
         elif len(p) == 4:
             p[0] = ast.DotAccessor(p[1], p[3])
         else:
